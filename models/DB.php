e78fc684e7c8a230c3b719df8b500b405fbc73bc
Felipe Chacon <felipipe88806@gmail.com>
2021-03-29 17:39:53 -0500

    [FIX] Deploy

--- models/DB.php
+++ models/DB.php
@@ -3,7 +3,7 @@
 class DB{
     public static function conectar(){
         try{
-            $pdo = new pdo('mysql: host=localhost; dbname=paginabanda; charset=utf8','root','');
+            $pdo = new pdo('mysql:host=us-cdbr-east-03.cleardb.com;dbname=heroku_1760f604f86fa74; charset=utf8','b489cc25e2060d','1d9bd5b1');
             $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
             return $pdo;
         }catch(Exception $e){
